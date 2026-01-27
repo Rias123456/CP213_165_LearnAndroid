685ce48a611b4abd510db257918c37b86b188fda
Rias123456 <diray258@gmail.com>
2026-01-27 16:22:13 +0700

    add hp

--- lab/app/src/main/java/com/example/a165lableandandriod/MainActivity2.kt
+++ lab/app/src/main/java/com/example/a165lableandandriod/MainActivity2.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue     // สำคัญ: เพื่อให้ใช้ 'by' อ่านค่าได้
+import androidx.compose.runtime.setValue     // สำคัญ: เพื่อให้ใช้ 'by' เขียนค่าได้
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a165lableandandriod.ui.theme._165LabLeandAndriodTheme
 
 class MainActivity2 : ComponentActivity() {
@@ -32,16 +38,21 @@ class MainActivity2 : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    var inputText by remember { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    _165LabLeandAndriodTheme {
-        Greeting2("Android")
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "สวัสดี $name! ข้อความคือ: $inputText",
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        TextField(
+            value = inputText,
+            onValueChange = { newValue ->
+                inputText = newValue
+            },
+
+
+        )
     }
 }
